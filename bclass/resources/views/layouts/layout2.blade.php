595a55d8efb7e0beb09bbe45d9f6a65315cb2cc5
E4CGYO <didanpradifta@gmail.com>
2025-11-04 13:16:19 +0800

    Update layout2.blade.php

--- bclass/resources/views/layouts/layout2.blade.php
+++ bclass/resources/views/layouts/layout2.blade.php
@@ -61,13 +61,11 @@
 <div class="container">
   <footer class="py-3 my-4">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Didan pradfta 2301010076</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Rojib 2301010120</a></li>
+      
     </ul>
-    <p class="text-center text-muted">&copy; 2021 Company, Inc</p>
+    <p class="text-center text-muted">&copy; 2025 Didan X Rojib, Inc</p>
   </footer>
 </div>
 
