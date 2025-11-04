e3f8f7faa8d8c447073781bb499e446a785d294e
E4CGYO <didanpradifta@gmail.com>
2025-11-04 11:43:16 +0800

    perubahan

---
+++ bclass/app/Http/Controllers/BlogController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Post;
+
+class BlogController extends Controller
+{
+    // seperti komponen tp bukan komponen halaman posts yg plural
+   
+
+    // halaman home isinya posts banyak
+    public function home()
+    {
+        $posts = Post::data();
+        $totalposts=Post::count();
+        return view('home', compact('posts','totalposts'));
+    }
+
+    // single kalo diklik satu2
+    public function show($id)
+    {
+        $post = Post::caridata ($id);
+
+        if (!$post) {
+            abort(404);
+        }
+
+        return view('post', compact('post'));
+    }
+
+    //halaman tentang
+    public function about()
+    {
+        $info = [
+            'name' => 'deva',
+            'bio' => 'penulis.'
+        ];
+
+        return view('about', compact('info'));
+    }
+}
